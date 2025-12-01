87a1eff43b00a007630cd68fe27956130ef7cd39
Arangluv <ruhunsu3@naver.com>
2025-12-01 19:59:11 +0900

    add: join design

--- src/app/(frontend)/(page)/join/page.tsx
+++ src/app/(frontend)/(page)/join/page.tsx
@@ -1,11 +1,20 @@
 import { BrandLogo } from '@/config/Logo'
+import JoinForm from './_components/Form'
 
 export default function JoinPage() {
   return (
-    <section className="w-full h-screen bg-neutral-50 flex items-center justify-center py-12">
-      <div className="w-full max-w-2xl bg-white h-[300px] rounded-2xl flex flex-col">
-        <div className="w-full">
-          <BrandLogo width={140} height={40} className="w-[140px] h-[40px]" />
+    <section className="w-full bg-neutral-50 flex items-center justify-center py-12">
+      <div className="w-full max-w-2xl flex flex-col">
+        {/* Header */}
+        <div className="flex flex-col gap-4 justify-center items-center mb-8">
+          <div className="w-full flex justify-center">
+            <BrandLogo width={140} height={40} className="w-[140px] h-[40px]" />
+          </div>
+          <h1 className="text-3xl font-bold text-foreground-900">회원가입</h1>
+        </div>
+        {/* Form */}
+        <div className="bg-white flex flex-col w-full rounded-2xl  py-12 px-8 ">
+          <JoinForm />
         </div>
       </div>
     </section>
