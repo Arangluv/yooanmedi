300f8abc1796b7fe832c491ca38f5206aeb2a186
Arangluv <ruhunsu3@naver.com>
2025-12-02 16:42:40 +0900

    add: join logic -> server actions

---
+++ src/collections/Files.ts
@@ -0,0 +1,18 @@
+import type { CollectionConfig } from 'payload'
+
+export const Files: CollectionConfig = {
+  slug: 'files',
+  access: {
+    read: () => true,
+    create: () => true,
+  },
+  admin: {
+    group: '파일 & 이미지',
+  },
+  labels: {
+    singular: '파일',
+    plural: '파일',
+  },
+  upload: {},
+  fields: [],
+}
