638f30fcec01f008cb4bf2786df28b965725589b
Arangluv <ruhunsu3@naver.com>
2025-12-15 13:23:13 +0900

    modify: 주문내역

--- src/app/(frontend)/(page)/order/(page)/list/actions.ts
+++ src/app/(frontend)/(page)/order/(page)/list/actions.ts
@@ -1,6 +1,6 @@
 'use server'
 
-import { getPayload } from 'payload'
+import { getPayload, Where } from 'payload'
 import config from '@/payload.config'
 import moment from 'moment'
 
@@ -8,12 +8,53 @@ export async function getOrderList({
   userId,
   start,
   end,
+  productName,
 }: {
   userId: string
   start: string
   end: string
+  productName: string
 }) {
   const payload = await getPayload({ config: config })
+  const startDate = new Date(start).toISOString()
+  const endDate = moment(end).endOf('day').toISOString()
+
+  let productIds: string[] | undefined
+  if (productName && productName.trim() !== '') {
+    const products = await payload.find({
+      collection: 'product',
+      where: {
+        name: {
+          contains: productName,
+        },
+      },
+      limit: 1000, // 충분히 큰 값으로 설정
+    })
+    productIds = products.docs.map((doc) => doc.id.toString())
+
+    // productName으로 검색된 결과가 없으면 빈 배열 반환
+    if (productIds.length === 0) {
+      return []
+    }
+  }
+
+  // where 조건 구성
+  const where: Where = {
+    user: {
+      equals: userId,
+    },
+    orderCreatedAt: {
+      greater_than_equal: startDate,
+      less_than_equal: endDate,
+    },
+  }
+
+  if (productIds && productIds.length > 0) {
+    where.product = {
+      in: productIds,
+    }
+  }
+
   const data = await payload.find({
     collection: 'order',
     select: {
@@ -37,16 +78,11 @@ export async function getOrderList({
         username: true,
       },
     },
-    where: {
-      user: {
-        equals: userId,
-      },
-      orderCreatedAt: {
-        greater_than_equal: new Date(start).toISOString(),
-        less_than_equal: moment(end).endOf('day').toISOString(),
-      },
-    },
+    where: where,
   })
+
+  console.log('data.docs')
+  console.log(data.docs)
 
   return data.docs
 }
