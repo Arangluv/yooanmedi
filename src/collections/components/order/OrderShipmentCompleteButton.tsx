b352b902c0d7e242d19b329af2d53d190cfb468a
Arangluv <ruhunsu3@naver.com>
2026-01-05 20:41:27 +0900

    add: bulk edit

--- src/collections/components/order/OrderShipmentCompleteButton.tsx
+++ src/collections/components/order/OrderShipmentCompleteButton.tsx
@@ -1,9 +1,10 @@
 'use client'
+
 import { toast, useSelection, useListQuery } from '@payloadcms/ui'
 import { useMutation } from '@tanstack/react-query'
 import { useEffect, useState } from 'react'
-import { updateOrderStatus } from './actions'
 import { useRouter } from 'next/navigation'
+import { updateOrderStatusToShipmentComplete } from './actions'
 
 interface SelectedData {
   id: number
@@ -20,15 +21,15 @@ interface SelectedData {
   createdAt: string
 }
 
-export default function OrderPaymentPendingToReadyButton() {
+export default function OrderShipmentCompleteButton() {
   const router = useRouter()
   const [disabledMessage, setDisabledMessage] = useState('')
   const [selectedData, setSelectedData] = useState<SelectedData[]>([])
   const { selected, toggleAll } = useSelection()
   const { data: listData } = useListQuery()
 
-  const { mutate: updateOrderStatusMutation } = useMutation({
-    mutationFn: (data: SelectedData[]) => updateOrderStatus(data),
+  const { mutate: updateOrderStatusToShipmentStartMutation } = useMutation({
+    mutationFn: (data: SelectedData[]) => updateOrderStatusToShipmentComplete(data),
     onSuccess: (data: { message: string; success: boolean }) => {
       if (data.success) {
         toast.success(data.message)
@@ -39,28 +40,21 @@ export default function OrderPaymentPendingToReadyButton() {
       }
     },
     onError: () => {
-      toast.error('상품준비 단계로 변경에 실패했습니다.')
+      toast.error('배송 완료 단계로 변경에 실패했습니다.')
     },
   })
-
   useEffect(() => {
     if (listData && listData.docs.length > 0) {
       const keys = Array.from(selected.entries())
         .filter(([_, value]) => value)
         .map(([key, _]) => key)
+
       const filterdData = listData.docs.filter((doc) => keys.includes(doc.id))
-      const isIncludeCard = filterdData.some((doc) => doc.paymentsMethod === 'creditCard')
-      const isIncludeNotValidStatus = filterdData.some((doc) => doc.orderStatus !== 5)
-      if (isIncludeCard) {
-        setDisabledMessage(
-          '신용카드 결제 주문이 포함되어 있습니다. 무통장 입금 주문만 상품준비 단계로 변경할 수 있습니다.',
-        )
-        return
-      }
+      const isIncludeNotValidStatus = filterdData.some((doc) => doc.orderStatus !== 2)
 
       if (isIncludeNotValidStatus) {
         setDisabledMessage(
-          '주문상태가 결제대기가 아닌 주문이 포함되어 있습니다. 결제대기 주문만 상품준비 단계로 변경할 수 있습니다.',
+          '주문상태가 배송 시작 단계가 아닌 주문이 포함되어 있습니다. 배송 시작 단계 주문만 배송 완료할 수 있습니다.',
         )
         return
       }
@@ -81,13 +75,16 @@ export default function OrderPaymentPendingToReadyButton() {
       return
     }
 
-    updateOrderStatusMutation(selectedData)
+    updateOrderStatusToShipmentStartMutation(selectedData)
   }
 
   return (
     <div>
-      <button className="bg-brand text-white px-3 py-1 rounded-md cursor-pointer" onClick={onClick}>
-        상품준비 단계로 변경 (무통장입금)
+      <button
+        className="bg-green-700 text-white px-3 py-1 rounded-md cursor-pointer"
+        onClick={onClick}
+      >
+        배송 완료 단계로 변경
       </button>
     </div>
   )
--- src/collections/components/order/actions.ts
+++ src/collections/components/order/actions.ts
@@ -2,7 +2,7 @@
 
 import { getPayload } from "payload"
 import config from "@/payload.config"
-import { BasePayload } from "payload"
+import { cancelOrderForCard, cancelOrderForBankTransfer } from "@order/(page)/list/actions"
 
 type SelectedData = {
   id: number
@@ -76,5 +76,91 @@ export async function updateOrderStatus(selectedData: SelectedData[]) {
     return {message: '상품준비 단계로 변경되었습니다.', success: true}
   } catch(error) {
     return {message: '상품준비 단계로 변경에 실패했습니다.', success: false}
+  }
+}
+
+export async function updateOrderStatusToShipmentStart(selectedData: SelectedData[]) {
+  const payload = await getPayload({ config: config })
+
+  try {
+    await Promise.all([
+      ...selectedData.map(async (data) => {
+        return payload.update({
+          collection: 'order',
+          id: data.id,
+          data: {
+            orderStatus: 2,
+          },
+        })
+      }),
+    ])
+
+    return {message: '배송 시작 단계로 변경되었습니다.', success: true}
+  } catch(error) {
+    return {message: '배송 시작 단계로 변경에 실패했습니다.', success: false}
+  }
+}
+
+export async function updateOrderStatusToShipmentComplete(selectedData: SelectedData[]) {
+    const payload = await getPayload({ config: config })
+  
+    try {
+      await Promise.all([
+        ...selectedData.map(async (data) => {
+          return payload.update({
+            collection: 'order',
+            id: data.id,
+            data: {
+              orderStatus: 3,
+            },
+          })
+        }),
+      ])
+  
+      return {message: '배송 완료 단계로 변경되었습니다.', success: true}
+    } catch(error) {
+      return {message: '배송 완료 단계로 변경에 실패했습니다.', success: false}
+    }
+  }
+
+export async function updateOrderStatusToCancel(selectedData: {bankTransferData: SelectedData[], cardData: SelectedData[]}) {
+  const payload = await getPayload({ config: config })
+  try {
+    // 주문상태 변경
+    await Promise.all([
+      ...selectedData.bankTransferData.map(async (data) => {
+        return payload.update({
+          collection: 'order',
+          id: data.id,
+          data: {
+            orderStatus: 4,
+          },
+        })
+      }),
+      ...selectedData.cardData.map(async (data) => {
+        return payload.update({
+          collection: 'order',
+          id: data.id,
+          data: {
+            orderStatus: 4,
+          },
+        })
+      }),
+    ])
+
+    // 주문취소 실행 -> 순서대로 실행해야함 (적립문제로 -> Promise.all 사용x)
+    for (const data of selectedData.bankTransferData) {
+      await cancelOrderForBankTransfer({ orderId: data.id, orderStatus: data.orderStatus })
+    }
+
+    for (const data of selectedData.cardData) {
+      await cancelOrderForCard({ orderId: data.id })
+    }
+
+
+
+    return {message: '주문취소 단계로 변경되었습니다.', success: true}
+  } catch (error) {
+    return {message: '주문취소 단계로 변경에 실패했습니다.', success: false}
   }
 }
