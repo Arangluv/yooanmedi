fdfb2c30fc0abf311643359e825e251c7e89c3b8
Arangluv <ruhunsu3@naver.com>
2026-01-06 10:32:16 +0900

    modify: cell

--- src/app/(payload)/admin/importMap.js
+++ src/app/(payload)/admin/importMap.js
@@ -1,3 +1,4 @@
+import { default as default_f28a02750c45e1c8fbf74ae7fa034c70 } from '@/collections/components/common/TextCell'
 import { default as default_7712bd14948acfabe5d33e1eef7b36b9 } from '@collections/components/product/ProductListTest'
 import { default as default_19920d989ded0e59fd7473c2d9c798ce } from '@/collections/components/order/OrderComponents'
 import { RscEntryLexicalCell as RscEntryLexicalCell_44fe37237e0ebf4470c9990d8cb7b07e } from '@payloadcms/richtext-lexical/rsc'
@@ -31,6 +32,7 @@ import { BrandLogoSmall as BrandLogoSmall_97bc79299977fcd966caf47fe3f27eaa } fro
 import { S3ClientUploadHandler as S3ClientUploadHandler_f97aa6c64367fa259c5bc0567239ef24 } from '@payloadcms/storage-s3/client'
 
 export const importMap = {
+  "@/collections/components/common/TextCell#default": default_f28a02750c45e1c8fbf74ae7fa034c70,
   "@collections/components/product/ProductListTest#default": default_7712bd14948acfabe5d33e1eef7b36b9,
   "@/collections/components/order/OrderComponents#default": default_19920d989ded0e59fd7473c2d9c798ce,
   "@payloadcms/richtext-lexical/rsc#RscEntryLexicalCell": RscEntryLexicalCell_44fe37237e0ebf4470c9990d8cb7b07e,
