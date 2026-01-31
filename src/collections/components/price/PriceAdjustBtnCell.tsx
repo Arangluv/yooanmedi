cf69c1ba8614ff5f09ee99806dad6c689c13d4af
Arangluv <ruhunsu3@naver.com>
2026-01-31 19:48:34 +0900

    feature: 유저 정보 불러오기 zustand 추가

--- src/collections/components/price/PriceAdjustBtnCell.tsx
+++ src/collections/components/price/PriceAdjustBtnCell.tsx
@@ -2,13 +2,22 @@
 
 import { DialogTrigger } from '@collections/components/shadcn';
 import { Button } from '@collections/components/shadcn';
+import useUserInfo from '@/app/(payload)/context/useUserInfo';
 
 export default function PriceAdjustBtnCell(props: any) {
-  const { cellData, rowData, field } = props;
+  const setUser = useUserInfo((state) => state.setUser);
+  const { rowData } = props;
+  const { hospitalName, ceo, email } = rowData;
 
   if (rowData.role === 'admin') {
     return null;
   }
+
+  setUser({
+    hosipital_name: hospitalName,
+    ceo_name: ceo,
+    email: email,
+  });
 
   return (
     <DialogTrigger asChild>
