6f5816707ea9f57119aa84ac0ccdec7300c19a45
Arangluv <ruhunsu3@naver.com>
2026-02-04 22:36:46 +0900

    refactor: product, modal 부분 FSD migration

--- src/shared/index.ts
+++ src/shared/index.ts
@@ -2,3 +2,11 @@
 export { getPayload } from './lib/get-payload';
 export { formatNumberWithCommas } from './lib/fomatters';
 export { isPayloadImageRenderable } from './lib/validation';
+
+// model
+export { default as useSiteMetaStore } from './model/useSiteMetaStore';
+export { default as SiteMetadataSetter } from './model/site-metadata-setter';
+export type { SiteMetadata } from './api/get-site-metadata';
+
+// api
+export { getSiteMetadata } from './api/get-site-metadata';
