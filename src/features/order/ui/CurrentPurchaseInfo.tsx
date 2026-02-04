d5416afbb0e409b9f811003de082caf4d5a63397
Arangluv <ruhunsu3@naver.com>
2026-02-04 16:31:46 +0900

    refactor: 기존 searchParams 관리 함수 및 타입 삭제, 페이지 동기화 작업

--- src/features/order/ui/CurrentPurchaseInfo.tsx
+++ src/features/order/ui/CurrentPurchaseInfo.tsx
@@ -4,15 +4,14 @@ import { useQuery } from '@tanstack/react-query';
 import moment from 'moment';
 
 import { getCurrentUserOrder } from '../api/get-current-user-order';
-import { ProductItem } from '@/entities/product/model/types';
 import { formatNumberWithCommas } from '@/shared/lib/fomatters';
+import type { ProductItem } from '@/entities/product/@x/order';
+import type { User } from '@/entities/user/@x/order';
 
-const CurrentPurchaseInfo = ({ product }: { product: ProductItem }) => {
-  const tempUserId = 1;
-
+const CurrentPurchaseInfo = ({ product, user }: { product: ProductItem; user: User }) => {
   const { data } = useQuery({
-    queryKey: ['order-history', product.id, tempUserId],
-    queryFn: () => getCurrentUserOrder({ prod_id: product.id, user_id: tempUserId }),
+    queryKey: ['order-history', product.id],
+    queryFn: () => getCurrentUserOrder({ prod_id: product.id, user_id: user.id }),
   });
 
   if (!data || data.length === 0) {
@@ -37,7 +36,7 @@ const CurrentPurchaseInfo = ({ product }: { product: ProductItem }) => {
                 {moment(item.orderCreatedAt).format('YYYY-MM-DD')}
               </td>
               <td className="border-foreground-200 border-r-1 text-center">{item.quantity}</td>
-              {/* TODO : 아직 entities에서 order type을 제대로 정의하지 않았음 */}
+              {/* TODO : refactoring -> isImageRenderable처럼 type 가드 필요 */}
               {/* @ts-ignore */}
               <td className="text-center">{formatNumberWithCommas(item.product?.price)}원</td>
             </tr>
