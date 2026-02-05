e3598e8ce498e5c0abc4a1d90f5cfb60f0d0064c
Arangluv <ruhunsu3@naver.com>
2026-02-05 18:03:05 +0900

    refactor: 결제 등록파트까지 FSD 마이그레이션

--- src/features/payments/model/payments-register-schema.ts
+++ src/features/payments/model/payments-register-schema.ts
@@ -1,44 +1,7 @@
 import * as z from 'zod';
 
-import type { InventoryItem } from '@/entities/inventory';
+import { PAYMENTS_METHOD } from '../constants/payments-options';
 
-type OrderInfo = {
-  goodsName: string;
-  customerInfo: {
-    customerId: string;
-    customerName: string;
-    customerMail: string;
-    customerContactNo: string;
-    customerAddr: string;
-  };
-};
-
-type ShopValueInfo = {
-  deliveryRequest: string; // value1
-  orderList: Array<{
-    product: Pick<InventoryItem['product'], 'id' | 'price'>;
-    quantity: InventoryItem['quantity'];
-  }>; // value2
-  usedPoint: number; // value3
-  userId: string;
-  paymentsMethod: 'creditCard' | 'bankTransfer';
-  minOrderPrice: number; // value6
-};
-
-type PaymentsRegisterDTO = {
-  mallId: string;
-  amount: number;
-  clientTypeCode: '00'; // 고정값
-  payMethodTypeCode: '11'; // 신용카드 결제만 지원하므로 고정
-  currency: '00'; // 원화 결제만 지원하므로 고정
-  deviceTypeCode: 'pc'; // 모바일 환경은 지원하지 않으므로 고정
-  returnUrl: string;
-  shopOrderNo: string;
-  orderInfo: OrderInfo;
-  shopValueInfo: ShopValueInfo;
-};
-
-// zod로 다시
 const orderInfoSchema = z.object({
   goodsName: z.string(),
   customerInfo: z.object({
@@ -50,36 +13,30 @@ const orderInfoSchema = z.object({
   }),
 });
 
-const shopValueInfoDtoSchema = z.object({
+const shopValueInfoApplicationDtoSchema = z.object({
   deliveryRequest: z.string(),
   orderList: z.array(
     z.object({
-      product: z.object({ id: z.string(), price: z.number() }),
+      product: z.object({ id: z.number(), price: z.number() }),
       quantity: z.number(),
     }),
   ),
   usedPoint: z.number(),
-  userId: z.string(),
-  paymentsMethod: z.enum(['creditCard', 'bankTransfer']),
+  userId: z.number(),
+  paymentsMethod: z.enum([PAYMENTS_METHOD.CREDIT_CARD, PAYMENTS_METHOD.BANK_TRANSFER]),
   minOrderPrice: z.number(),
 });
 
-const shopValueInfoEntitySchema = z.object({
+const shopValueInfoRequestDtoSchema = z.object({
   value1: z.string(),
-  value2: z.array(
-    z.object({
-      product: z.object({ id: z.string(), price: z.number() }),
-      quantity: z.number(),
-    }),
-  ),
+  value2: z.json(),
   value3: z.number(),
-  value4: z.string(),
-  value5: z.enum(['creditCard', 'bankTransfer']),
+  value4: z.number(),
+  value5: z.enum([PAYMENTS_METHOD.CREDIT_CARD, PAYMENTS_METHOD.BANK_TRANSFER]),
   value6: z.number(),
 });
 
 const paymentsRegisterBaseSchema = z.object({
-  mallId: z.string(),
   amount: z.number(),
   clientTypeCode: z.literal('00'),
   payMethodTypeCode: z.literal('11'),
@@ -90,12 +47,13 @@ const paymentsRegisterBaseSchema = z.object({
   orderInfo: orderInfoSchema,
 });
 
-export type PaymentsRegisterDto = z.output<typeof paymentsRegisterDtoSchema>;
-export const paymentsRegisterDtoSchema = paymentsRegisterBaseSchema.extend({
-  shopValueInfo: shopValueInfoDtoSchema,
+export type PaymentsRegisterApplicationDto = z.output<typeof paymentsRegisterApplicationDtoSchema>;
+export const paymentsRegisterApplicationDtoSchema = paymentsRegisterBaseSchema.extend({
+  shopValueInfo: shopValueInfoApplicationDtoSchema,
 });
 
-export type PaymentsRegisterBase = z.output<typeof paymentsRegisterEntitySchema>;
-export const paymentsRegisterEntitySchema = paymentsRegisterBaseSchema.extend({
-  shopValueInfo: shopValueInfoEntitySchema,
+export type PaymentsRegisterRequestDto = z.output<typeof paymentsRegisterRequestDtoSchema>;
+export const paymentsRegisterRequestDtoSchema = paymentsRegisterBaseSchema.extend({
+  mallId: z.string(),
+  shopValueInfo: shopValueInfoRequestDtoSchema,
 });
