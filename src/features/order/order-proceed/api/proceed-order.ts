3c8611384a341868f0f15555f2714930080412f6
Arangluv <ruhunsu3@naver.com>
2026-03-02 21:46:07 +0900

    refactor: update action -> proceed와 cancel로 구분, proceed part complete

---
+++ src/features/order/order-proceed/api/proceed-order.ts
@@ -0,0 +1,90 @@
+'use server';
+
+import { OrderAction } from '@/entities/order/constants/order-action';
+import { OrderStatus } from '@/entities/order/constants/order-status';
+import { OrderStateMachine } from '@/entities/order/model/order-state-machine';
+import { ProceedCommandFactory } from '../../order-proceed/model/proceed-command-factory';
+import { ExecuteActionResult } from '../model/types';
+
+// 딱 한개의 order status만 바꾸는 함수
+export const proceedSingleOrder = async (
+  action: OrderAction | null,
+  currentStatus: OrderStatus | null,
+  targetOrderId: number,
+): Promise<ExecuteActionResult> => {
+  try {
+    if (!action || !currentStatus || !targetOrderId) {
+      return {
+        success: false,
+        message: '[proceedOrder] 필수 파라미터가 누락되었습니다',
+      };
+    }
+
+    const orderStatusMachine = new OrderStateMachine(currentStatus as OrderStatus);
+    if (!orderStatusMachine.canExecuteAction(action as OrderAction)) {
+      return {
+        success: false,
+        message: '[orderStatusMachine] 해당 상태에서는 해당 액션을 수행할 수 없습니다',
+      };
+    }
+
+    const command = ProceedCommandFactory.createCommand(currentStatus as OrderStatus);
+    const result = await command.execute(targetOrderId);
+
+    return result;
+  } catch (error) {
+    const errorMessage = error instanceof Error ? error.message : '알 수 없는 오류가 발생했습니다';
+
+    return {
+      success: false,
+      message: errorMessage,
+    };
+  }
+};
+
+export const proceedMultipleOrder = async (
+  action: OrderAction | null,
+  currentStatus: OrderStatus | null,
+  targetOrderIds: number[],
+): Promise<ExecuteActionResult> => {
+  if (!action || !currentStatus || !targetOrderIds) {
+    return {
+      success: false,
+      message: '[proceedMultipleOrder] 필수 파라미터가 누락되었습니다',
+    };
+  }
+
+  const orderStatusMachine = new OrderStateMachine(currentStatus as OrderStatus);
+  if (!orderStatusMachine.canExecuteAction(action as OrderAction)) {
+    return {
+      success: false,
+      message: '[orderStatusMachine] 해당 상태에서는 해당 액션을 수행할 수 없습니다',
+    };
+  }
+
+  const command = ProceedCommandFactory.createCommand(currentStatus as OrderStatus);
+  const results = await Promise.allSettled(
+    targetOrderIds.map(async (targetOrderId) => {
+      const commandResult = await command.execute(targetOrderId);
+
+      if (!commandResult.success) {
+        return Promise.reject(commandResult.message);
+      }
+    }),
+  );
+
+  const successCount = results.filter((result) => result.status === 'fulfilled').length;
+  const failCount = results.length - successCount;
+
+  if (failCount > 0) {
+    return {
+      success: false,
+      message: `주문 진행 중 오류가 발생했습니다 (${failCount}건)`,
+    };
+  }
+
+  return {
+    success: true,
+    message: `주문 진행이 완료되었습니다 (${successCount}건)`,
+  };
+};
---
+++ src/features/order/order-proceed/model/command/base-command.ts
@@ -0,0 +1,88 @@
+import { BasePayload } from 'payload';
+import { getPayload } from '@/shared/lib/get-payload';
+import { ORDER_STATUS, OrderStatus } from '@/entities/order/constants/order-status';
+import { findAllOrderProduct } from '../../lib/find-all-order-product';
+import { ORDER_PRODUCT_STATUS } from '@/entities/order-product/constants/order-product-status';
+import { PAYMENT_STATUS } from '@/entities/order/constants/payment-status';
+
+export interface OrderCommand {
+  execute(targetOrderId: number): Promise<CommandResult>;
+}
+
+export interface CommandResult {
+  success: boolean;
+  message: string;
+}
+
+export abstract class BaseProceedCommand implements OrderCommand {
+  constructor() {
+    this.payload = null;
+    this.orderProductIds = [];
+  }
+
+  protected payload: BasePayload | null;
+  protected orderProductIds: number[];
+
+  protected setOrderProductIds(orderProductIds: number[]) {
+    if (orderProductIds.length === 0) {
+      throw new Error('주문 상품이 존재하지 않습니다');
+    }
+
+    this.orderProductIds = orderProductIds;
+  }
+
+  protected async updateOrderStatus(orderId: number, toStatus: OrderStatus) {
+    if (!this.payload) {
+      throw new Error('payload 객체가 존재하지 않습니다');
+    }
+
+    const allProducts = await findAllOrderProduct(orderId);
+    const hasCancelRequest = allProducts.some(
+      (product) => product.orderProductStatus === ORDER_PRODUCT_STATUS.CANCEL_REQUEST,
+    );
+
+    const data = {
+      orderStatus: toStatus,
+    } as Record<string, string>;
+
+    // cancel request가 하나라도 존재할 시
+    if (hasCancelRequest) {
+      data.paymentStatus = PAYMENT_STATUS.PARTIAL_CANCEL;
+      return;
+    }
+
+    const additionalData = this.getAdditionalOrderUpdateData();
+    Object.assign(data, additionalData);
+
+    await this.payload.update({
+      collection: 'order',
+      id: orderId,
+      data,
+    });
+  }
+
+  protected getAdditionalOrderUpdateData() {
+    return {};
+  }
+
+  abstract runProceed(targetOrderId: number): Promise<CommandResult>;
+
+  async execute(targetOrderId: number): Promise<CommandResult> {
+    try {
+      this.payload = await getPayload();
+      const result = await this.runProceed(targetOrderId);
+
+      return {
+        success: true,
+        message: result.message,
+      };
+    } catch (error) {
+      const errorMessage =
+        error instanceof Error ? error.message : '알 수 없는 오류가 발생했습니다';
+      return {
+        success: false,
+        message: errorMessage,
+      };
+    }
+  }
+}
