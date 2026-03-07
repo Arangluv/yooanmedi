a56d9f087b11f9e60288ab3fe4ffd12b81515211
Arangluv <ruhunsu3@naver.com>
2026-03-07 18:50:26 +0900

    refactor: [Admin] order detail -> order info part 중복코드 정리

--- src/features/admin/order-detail/config/order-detail-ui-config.ts
+++ src/features/admin/order-detail/config/order-detail-ui-config.ts
@@ -2,20 +2,24 @@ export type OrderInfomationCardType = 'progress' | 'cancelRequest' | 'cancelled'
 
 interface OrderInfomationCardConfig {
   title: string;
+  emptyContent: string;
   canItemCancel: boolean;
 }
 
 export const ORDER_DETAIL_UI_CONFIG: Record<OrderInfomationCardType, OrderInfomationCardConfig> = {
   progress: {
     title: '진행중인 주문',
+    emptyContent: '진행중인 주문이 없습니다.',
     canItemCancel: true,
   },
   cancelRequest: {
     title: '주문취소 요청',
+    emptyContent: '취소요청된 주문이 없습니다.',
     canItemCancel: false,
   },
   cancelled: {
     title: '취소된 주문',
+    emptyContent: '취소된 주문이 없습니다.',
     canItemCancel: false,
   },
 };
--- src/features/admin/order-detail/ui/order-info-card.tsx
+++ src/features/admin/order-detail/ui/order-info-card.tsx
@@ -1,127 +1,158 @@
-// 'use client';
+'use client';
 
-// import { Fragment } from 'react';
-// import moment from 'moment';
+import { Fragment } from 'react';
+import moment from 'moment';
 
-// import OrderStatusBadge from '@/entities/order/ui/admin/badge';
-// import { useOrderCollection } from '../model/order-provider';
-// import { ORDER_STATUS, PAYMENTS_METHOD } from '@/entities/order';
-// import { ORDER_ACTION } from '@/entities/order/constants/order-action';
-// import EmptyOrderInfo from '@/entities/order/ui/admin/EmptyOrderInfo';
-// import {
-//   Card,
-//   CardHeader,
-//   CardTitle,
-//   CardDescription,
-//   CardContent,
-//   CardFooter,
-// } from '@/shared/ui/shadcn/card';
-// import { ItemGroup, ItemSeparator } from '@/shared/ui/shadcn/item';
+import OrderStatusBadge from '@/entities/order/ui/admin/badge';
+import { useOrderCollection } from '../model/order-provider';
+import { ORDER_STATUS } from '@/entities/order';
+import EmptyOrderInfo from '@/entities/order/ui/admin/EmptyOrderInfo';
+import { OrderStatus } from '@/entities/order/constants/order-status';
+import {
+  Card,
+  CardHeader,
+  CardTitle,
+  CardDescription,
+  CardContent,
+  CardFooter,
+} from '@/shared/ui/shadcn/card';
+import { ItemGroup, ItemSeparator } from '@/shared/ui/shadcn/item';
 
-// import OrderProductItem from './OrderProductItem';
-// import { OrderAction } from '../model/order-action-dialog-provider';
-// import { ORDER_DETAIL_UI_CONFIG, OrderInfomationCardType } from '../config/order-detail-ui-config';
+import OrderProductItem from './OrderProductItem';
+import { OrderAction } from '../model/order-action-dialog-provider';
+import { ORDER_DETAIL_UI_CONFIG, OrderInfomationCardType } from '../config/order-detail-ui-config';
+import { OrderProduct } from '../model/order-detail-schema';
 
-// interface OrderInfoCardProps {
-//   type: OrderInfomationCardType;
-//   children?: React.ReactNode;
-// }
+interface OrderInfoCardProps {
+  type: OrderInfomationCardType;
+  orderId: number;
+  orderStatus: OrderStatus;
+  orderNo: string;
+  date: string;
+  orderProducts: OrderProduct[];
+  children?: React.ReactNode;
+}
 
-// const OrderInfoCard = ({ type }: OrderInfoCardProps) => {
-//   const uiConfig = ORDER_DETAIL_UI_CONFIG[type];
+const OrderInfoCard = ({
+  type,
+  orderStatus,
+  orderNo,
+  date,
+  orderProducts,
+  orderId,
+  children,
+}: OrderInfoCardProps) => {
+  const uiConfig = ORDER_DETAIL_UI_CONFIG[type];
+  const isEmpty = orderProducts.length === 0;
 
-//   return (
-//     <Card>
-//       <CardHeader>
-//         <CardTitle className="flex items-center justify-between text-xl">
-//           <span>{uiConfig.title}</span>
-//           {orderInfo?.progressOrder?.orderStatus && (
-//             <OrderStatusBadge orderStatus={orderInfo.progressOrder.orderStatus} />
-//           )}
-//         </CardTitle>
-//         <CardDescription className="flex flex-row gap-2 text-lg">
-//           <span>#{orderInfo?.progressOrder?.orderNo}</span>•
-//           <span>{moment(orderInfo?.progressOrder?.date).format('YYYY-MM-DD HH:mm:ss')} 생성</span>
-//         </CardDescription>
-//       </CardHeader>
-//       <CardContent>
-//         <ItemGroup>
-//           {isEmpty ? (
-//             <EmptyOrderInfo title="주문 정보가 없습니다." />
-//           ) : (
-//             orderInfo?.progressOrder?.orderProducts.map((orderProduct, idx) => {
-//               return (
-//                 <Fragment key={orderProduct.id}>
-//                   <OrderProductItem
-//                     orderId={orderInfo.progressOrder.id}
-//                     idx={idx}
-//                     orderProduct={orderProduct}
-//                     isCancelAction={true}
-//                   />
-//                   <ItemSeparator />
-//                 </Fragment>
-//               );
-//             })
-//           )}
-//         </ItemGroup>
-//       </CardContent>
-//       {isEmpty ? null : (
-//         <CardFooter className="justify-end">
-//           {orderInfo?.progressOrder?.orderStatus && (
-//             <Fragment>
-//               <OrderAction.ProceedTrigger
-//                 display={{
-//                   count: orderInfo.progressOrder.orderProducts.length,
-//                   viewType: 'order-detail',
-//                 }}
-//                 targetOrderIds={[orderInfo.progressOrder.id]}
-//                 currentStatus={orderInfo.progressOrder.orderStatus}
-//               />
-//               <OrderAction.ProceedContent />
-//             </Fragment>
-//           )}
-//         </CardFooter>
-//       )}
-//     </Card>
-//   );
-// };
+  return (
+    <Card>
+      <CardHeader>
+        <CardTitle className="flex items-center justify-between text-xl">
+          <span>{uiConfig.title}</span>
+          <OrderStatusBadge orderStatus={orderStatus} />
+        </CardTitle>
+        <CardDescription className="flex flex-row gap-2 text-lg">
+          <span>#{orderNo}</span>•<span>{moment(date).format('YYYY-MM-DD HH:mm:ss')} 생성</span>
+        </CardDescription>
+      </CardHeader>
+      <CardContent>
+        {isEmpty ? (
+          <EmptyOrderInfo title="주문 정보가 없습니다." />
+        ) : (
+          <ItemGroup>
+            {orderProducts.map((orderProduct, idx) => {
+              return (
+                <Fragment key={orderProduct.id}>
+                  <OrderProductItem
+                    orderId={orderId}
+                    idx={idx}
+                    orderProduct={orderProduct}
+                    isCancelAction={uiConfig.canItemCancel}
+                  />
+                  <ItemSeparator className="last:hidden" />
+                </Fragment>
+              );
+            })}
+          </ItemGroup>
+        )}
+      </CardContent>
+      {children}
+    </Card>
+  );
+};
 
-// export const OrderProgressInfoCard = () => {
-//   return (
-//     <OrderInfoCard type="progress">
-//       {/* <CardFooter className="justify-end">
-//         <OrderAction.ProceedTrigger
-//           display={{
-//             count: orderInfo.progressOrder.orderProducts.length,
-//             viewType: 'order-detail',
-//           }}
-//           targetOrderIds={[orderInfo.progressOrder.id]}
-//           currentStatus={orderInfo.progressOrder.orderStatus}
-//         />
-//         <OrderAction.ProceedContent />
-//       </CardFooter> */}
-//     </OrderInfoCard>
-//   );
-// };
+export const OrderProgressInfoCard = () => {
+  const {
+    orderInfo: { progressOrder, orderNo, createdAt, id: orderId },
+  } = useOrderCollection();
 
-// export const OrderCancelRequestInfoCard = () => {
-//   return (
-//     <OrderInfoCard type="cancelRequest">
-//       {/* <CardFooter className="justify-end">
-//         <OrderAction.CancelTrigger
-//           display={{
-//             count: orderInfo?.cancelRequestOrder?.orderProducts.length ?? 0,
-//             viewType: 'order-detail',
-//           }}
-//           targetOrderIds={[orderInfo?.cancelRequestOrder?.id ?? 0]}
-//           currentStatus={ORDER_STATUS.CANCEL_REQUEST}
-//         />
-//         <OrderAction.CancelContent />
-//       </CardFooter> */}
-//     </OrderInfoCard>
-//   );
-// };
+  return (
+    <OrderInfoCard
+      type="progress"
+      orderId={orderId}
+      orderStatus={progressOrder.inProgressOrderStatus}
+      orderNo={orderNo}
+      date={createdAt}
+      orderProducts={progressOrder.orderProducts}
+    >
+      <CardFooter className="justify-end">
+        <OrderAction.ProceedTrigger
+          display={{
+            count: progressOrder.orderProducts.length,
+            viewType: 'order-detail',
+          }}
+          targetOrderIds={[orderId]}
+          currentStatus={progressOrder.inProgressOrderStatus}
+        />
+        <OrderAction.ProceedContent />
+      </CardFooter>
+    </OrderInfoCard>
+  );
+};
 
-// export const OrderCancelledInfoCard = () => {
-//   return <OrderInfoCard type="cancelled" />;
-// };
+export const OrderCancelRequestInfoCard = () => {
+  const {
+    orderInfo: { cancelRequestOrder, orderNo, updatedAt, id: orderId },
+  } = useOrderCollection();
+
+  return (
+    <OrderInfoCard
+      type="cancelRequest"
+      orderId={orderId}
+      orderStatus={ORDER_STATUS.CANCEL_REQUEST}
+      date={updatedAt}
+      orderNo={orderNo}
+      orderProducts={cancelRequestOrder.orderProducts}
+    >
+      <CardFooter className="justify-end">
+        <OrderAction.CancelTrigger
+          display={{
+            count: cancelRequestOrder.orderProducts.length,
+            viewType: 'order-detail',
+          }}
+          targetOrderIds={[orderId]}
+          currentStatus={ORDER_STATUS.CANCEL_REQUEST}
+        />
+        <OrderAction.CancelContent />
+      </CardFooter>
+    </OrderInfoCard>
+  );
+};
+
+export const OrderCancelledInfoCard = () => {
+  const {
+    orderInfo: { cancelledOrder, orderNo, updatedAt, id: orderId },
+  } = useOrderCollection();
+
+  return (
+    <OrderInfoCard
+      type="cancelled"
+      orderId={orderId}
+      orderStatus={ORDER_STATUS.CANCELLED}
+      date={updatedAt}
+      orderNo={orderNo}
+      orderProducts={cancelledOrder.orderProducts}
+    />
+  );
+};
